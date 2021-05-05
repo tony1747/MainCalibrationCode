2a0cd099fe317cba9baec3559f96e46a1fb9a3e0
tony1747 <64659546+tony1747@users.noreply.github.com>
2021-05-04 17:38:24 -0700

    Add files via upload

--- read_CARTdata.m
+++ read_CARTdata.m
@@ -1,8 +1,10 @@
 %% code to read excel files 
 
-cancerdata = xlsread( 'Run1_HT1080.xlsx', 'Sheet1' ); 
-%cancerdata = xlsread( 'Run8_PBT138.xlsx', 'Sheet1' ); 
-
+%cancerdata = xlsread( 'Run1_HT1080.xlsx', 'Sheet1' ); 
+cancerdata = xlsread( 'Run8_PBT138.xlsx', 'Sheet1' ); 
+receptor = {'L','L','L','M','M','M','H','H','H','VH','VH','VH'};
+receptor = repmat(receptor,1,8);
+cartinit= [repmat(0,1,12),repmat(5,1,12),repmat(10,1,12),repmat(20,1,12),repmat(20,1,12),repmat(10,1,12),repmat(5,1,12),repmat(0,1,12)]; %#ok<REPMAT>
 
 % Time in first column (in sec) 
 time = cancerdata(:,1); 
