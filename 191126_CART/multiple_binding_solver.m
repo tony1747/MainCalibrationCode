cf7e00022efcee8219fd63e1677c58877bca4eaa
tony1747 <64659546+tony1747@users.noreply.github.com>
2021-06-15 19:02:36 -0700

    Add files via upload

--- 191126_CART/multiple_binding_solver.m
+++ 191126_CART/multiple_binding_solver.m
@@ -1,22 +1,22 @@
-function multiple_binding_solver(string)%for example, string='H, 1:10, 1'
-[num,txt,raw]=xlsread('fitted params.xlsx');
+function multiple_binding_solver(choice)%for example, choice='H, 1:10, 1'
+[num,txt,raw]=xlsread('fitted params(PBT138, slow).xlsx');
 
-row=find(strcmp(txt,string));
+row=find(strcmp(txt,choice));
 params=num(row,:);
-receptor=string(1);
-ratio=extractBetween(string,' ',',','Boundaries','exclusive');
-if ratio{1}=='1:5'
+receptor=choice(1);
+ratio=extractBetween(choice,' ',',','Boundaries','exclusive');%extract 1:10 out of the choice
+if string(ratio{1})=='1:5'
     CARTnum=5;    
-elseif ratio{1}=='1:10'
+elseif string(ratio{1})=='1:10'
     CARTnum=10;   
-elseif ratio{1}=='1:20'
+elseif string(ratio{1})=='1:20'
     CARTnum=20;
 end
 CARTratio=1/CARTnum;
 read_CARTdata()
-n=str2double(string(end));
+n=str2double(choice(end));  %choose the index of 'H, 1:10, 1', 
 data=selected_cancerdata(n);
 x(1)=data(1);
-x(2)=data(1)/CARTratio;
+x(2)=data(1)*CARTratio;
 x(3:4)=0;
 [t,tumVol] = ode23(@(t,y)two_binding_slow(t,y,param), x, time, params );
